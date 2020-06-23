85d68e54e3f800a4221526db632cf45e18167b15
Gonzalo Munoz <gonzalo.munoz@globant.com>
2020-06-23 16:16:31 -0400

    Adds communication with firebase

---
+++ app/src/main/java/tech/nullpointerexception/conf/model/Speaker.kt
@@ -0,0 +1,11 @@
+package tech.nullpointerexception.conf.model
+
+class Speaker{
+    var name = ""
+    var jobtitle = ""
+    var workplace = ""
+    var biography = ""
+    var twitter = ""
+    var image = ""
+    var category = 0
+}
---
+++ app/src/main/java/tech/nullpointerexception/conf/model/Ubication.kt
@@ -0,0 +1,11 @@
+package tech.nullpointerexception.conf.model
+
+class Ubication {
+    val name = ""
+    val address = ""
+    val latitude = ""
+    val longitude =""
+    val phone = ""
+    val website = ""
+    val photo = ""
+}
